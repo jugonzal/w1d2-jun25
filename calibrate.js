b9ca683b4e6d5878db7c26d8eeee74fb420e65ce
Juan Gonzalez <juan@lighthouselabs.com>
2019-06-25 15:29:03 +0000

    got first version running with one test case

--- calibrate.js
+++ calibrate.js
@@ -1,3 +1,14 @@
 // collect a bunch of numbers
 // add them up
 // display final result
+
+let input = process.argv.slice(2);
+let total = 0;
+
+for (i = 0; i < input.length; i++) {
+  console.log("i: ",i, parseInt(input[i]));
+  total = total + parseInt(input[i]);
+  console.log("total: ", total);
+}
+
+console.log("Result: ", total)
